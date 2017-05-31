a717e8c9a53e20802b6fa7a9d1e8d3c6b3b913b6
michaelb <michaelpb@gmail.com>
2017-05-31 12:53:32 -0700

    working on convert cli

--- omnithumb/commands.py
+++ omnithumb/commands.py
@@ -5,6 +5,14 @@ import click
 
 import omnithumb
 from omnithumb import default_settings as settings
+from omnithumb.converter.utils import convert as async_convert
+from omnithumb.types.typestring import TypeString
+
+def get_config():
+    extra_settings = os.environ.get('OMNIC_SETTINGS')
+    if extra_settings:
+        # TODO import here
+        pass
 
 @click.group()
 def cmds():
@@ -19,21 +27,22 @@ def cmds():
               help=u'Set application server debug')
 def runserver(port, ip, debug):
     click.echo('Start server at: {}:{}'.format(ip, port))
+    # TODO: add reloading, add environ 
     omnithumb.runserver(settings, host=ip, port=port, debug=debug)
 
-    register_all()
-    # TODO: add reloading, add environ 
-    app.run(host=ip, port=port, debug=debug)
-
-async def convert(in_file, out_type):
-    pass
+    #register_all()
+    #app.run(host=ip, port=port, debug=debug)
 
 @cmds.command()
 @click.argument('file', required=True)
 @click.argument('type', required=True)
-def convert(file, type):
+def convert(path, to_type):
     # TODO: add running of tests
-    pass
+    if not path.startswith('/'):
+        dirname = path.dirname(__file__)
+        path = os.path.resolve(dirname, path)
+    to_type = TypeString(path)
+    asyncio.ensure_future(async_convert(path, to_type))
 
 def main():
     cmds()
---
+++ omnithumb/conversion/utils.py
@@ -0,0 +1,41 @@
+import asyncio
+from omnithumb.types.resource import TypedResource, ForeignResource, TypedLocalResource
+
+async def convert(config, uri, to_type, enqueue_method=None, download_method=None):
+    if uri.startswith('/'):
+        # File protocol, 
+        typed_foreign_res = TypedLocalResource(config, uri)
+        resource_class = TypedLocalResource
+    else:
+        # Need to first download (blocking for now :/)
+        foreign_res = ForeignResource(config, uri)
+        if foreign_res.exists():
+            foreign_res.download()
+
+        resource_class = TypedResource
+
+        # Determine the file type of the foreign resource
+        typed_foreign_res = foreign_res.guess_typed()
+
+        if not typed_foreign_res.cache_exists():
+            # Symlink to new location that includes typed extension
+            typed_foreign_res.symlink_from(foreign_res)
+
+    # Now find path between types
+    original_ts = typed_foreign_res.typestring
+    path = config.converter_graph.find_path(original_ts, target_ts)
+
+    # Loop through each step in graph path and convert
+    is_first = True
+    for converter_class, from_ts, to_ts in path:
+        converter = converter_class(config)
+        in_resource = resource_class(config, uri, from_ts)
+        if is_first: # Ensure first resource is just the source one
+            in_resource = typed_foreign_res
+        out_resource = resource_class(config, uri, to_ts)
+        if enqueue_method:
+            enqueue_method(converter, in_resource, out_resource)
+        else:
+            await converter.convert(in_resource, out_resource)
+        is_first = False
+
